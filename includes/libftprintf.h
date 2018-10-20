efdf6655a3cd6dfc3d7c26f0845b06e59dd682b7
Emilien Baudet <emilien.baudet@gmail.com>
2018-10-20 16:33:31 +0200

    add include libft + add ft_sprintf function

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -24,20 +24,7 @@ typedef struct		s_ftprintf
 	int				count;
 }					t_ftprintf;
 
-int		ft_atoi(const char *str);
-char	*ft_lutohex(long unsigned int lu);
-size_t	ft_strlen(const char *s);
-void	ft_putchar(char c);
-char	*ft_itoa(int n);
-char	*ft_strcpy(char *s1, const char *s2);
-char	*ft_strdup(const char *s1);
-void	print_c(t_ftprintf *t);
-void	print_s(t_ftprintf *t);
-void	print_d(t_ftprintf *t);
-void	putstr_count(t_ftprintf *t, char *str);
-int		is_arg(t_ftprintf *t, char *format);
-void	print_uniq_caract(t_ftprintf *t, char *format);
-void	print_caract(t_ftprintf *t, char *format);
+char	*ft_sprintf(const char *format, ...);
 int		ft_printf(const char *format, ...);
 
 #endif /* LIBFTPRINTF_H */
