53db533fed58bc73f05206309b7c679ecc61e8db
Emilien Baudet <emilien.baudet@gmail.com>
2019-02-02 20:40:13 +0100

    change structure of project to deal with char c=0

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/21 20:45:55 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/02/02 16:33:34 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/02/02 20:36:49 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,93 +15,92 @@
 
 #include <stdio.h>
 
-static char	*is_arg(t_ftprintf *t, char *format, t_params *params)
+static int	is_arg(t_ftprintf *t, const char *format, t_params *params)
 {
-	char			buf[256];
 	char			*next_modulo;
 	char			*end_arg;
 
-	ft_memset(buf, 0, 256);
-	t->buf = buf;
 	if (format[t->i] == '%' && ++(t->i))
 	{
 		if (!format[t->i])
-			return (ft_strdup(ft_strncat(buf, "%", 1)));
+		{
+			ft_strncat(params->buf, "%", 1);
+			params->size = 1;
+			return (1);
+		}
 		next_modulo = ft_strchr(&format[t->i], '%');
-		end_arg = ft_strstrchr(&format[t->i], "cspdiouxXf");
+		end_arg = ft_strstrchr(&format[t->i], P_TYPE);
 		if (end_arg == NULL && next_modulo == NULL)
-			return (ft_strdup(ft_strncat(buf, "[FORMAT ERROR]", 14)));
+		{
+			ft_strncat(params->buf, "[FORMAT ERROR]", 14);
+			params->size = 14;
+			return (1);
+		}
 		while (format[t->i] && call_handler(format, t, params) > 0)
 			;
-		return (ft_strdup(buf));
+		return (1);
 	}
-	return (NULL);
+	return (0);
 }
 
-static char	*print_caract(t_ftprintf *t, char *format)
+void	ft_concat_param(t_ftprintf *t, t_params *params)
 {
-	char		buf[256];
-	char		*tmp;
-	t_params	params;
+	char	*tmp;
 
-	params_init(&params);
-	ft_memset(buf, 0, 256);
-	while (format[t->i])
-	{
-		tmp = is_arg(t, format, &params);
-		if (tmp != NULL)
-		{
-			ft_strcat(buf, tmp);
-			free(tmp);
-			break ;
-		}
-		ft_strncat(buf, format + t->i, 1);
-		t->i += 1;
-	}
-	return (ft_strdup(buf));
+	tmp = ft_memalloc(t->size + params->size);
+	ft_memcpy(tmp, t->str, t->size);
+	ft_memcpy(tmp + t->size, params->buf, params->size);
+	free(t->str);
+	t->size += params->size;
+	t->str = tmp;
 }
 
 static char	*ft_getsstr(t_ftprintf *t, const char *format)
 {
-	char	*str;
-	char	*tmp;
-	char	*buf;
+	t_params	params;
 
-	str = ft_memalloc(1);
+	params_reset(&params);
+	t->str = ft_memalloc(1);
 	t->i = 0;
 	while (format[t->i] != '\0')
 	{
-		tmp = print_caract(t, (char *)format);
-		buf = ft_strjoin(str, tmp);
-		free(str);
-		free(tmp);
-		str = buf;
+		if (!is_arg(t, format, &params))
+		{
+			params.buf[0] = format[t->i];
+			params.size = 1;
+			t->i++;
+		}
+		ft_concat_param(t, &params);
+		params_reset(&params);
 	}
-	return (str);
+	return (t->str);
 }
 
-char		*ft_sprintf(const char *format, ...)
+t_sprintf		*ft_sprintf(const char *format, ...)
 {
 	t_ftprintf	t;
 	char		*str;
+	t_sprintf	*out;
 
+	t.size = 0;
 	va_start(t.ap, format);
 	str = ft_getsstr(&t, format);
 	va_end(t.ap);
-	return (str);
+	out = ft_memalloc(sizeof(*out));
+	out->size = t.size;
+	out->str = t.str;
+	return (out);
 }
 
 int			ft_printf(const char *format, ...)
 {
 	t_ftprintf	t;
-	char		*str;
-	int			length;
 
+	t.size = 0;
 	va_start(t.ap, format);
-	str = ft_getsstr(&t, format);
+	ft_getsstr(&t, format);
 	va_end(t.ap);
-	length = ft_strlen(str);
-	ft_putstr(str);
-	free(str);
-	return (length);
+	write(1, t.str, t.size);
+	free(t.str);
+	return(t.size);
 }
